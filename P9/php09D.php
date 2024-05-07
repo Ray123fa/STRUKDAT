d6e121fcd51054cfe9c8cad918fc74e38afefb56
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-08 00:28:47 +0700

    update: pemisahan db config

--- P9/php09D.php
+++ P9/php09D.php
@@ -15,17 +15,7 @@
 	<h1>PHP and Databases</h1>
 
 	<?php
-	$db_hostname = "localhost";
-	$db_username = "root";
-	$db_password = "root";
-	$db_database = "pbw";
-
-	$dsn = "mysql:host=$db_hostname;dbname=$db_database";
-	$opt = array(
-		PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-		PDO::ATTR_EMULATE_PREPARES => false
-	);
+	require_once "db.php";
 
 	try {
 		$pdo = new PDO($dsn, $db_username, $db_password, $opt);
