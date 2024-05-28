ddaba8b3c64e02740d15eab49022a8963b32d9e7
Rayhan F <rayhanfrdh123@gmail.com>
2024-05-28 21:27:02 +0700

    Add P12

---
+++ P12/webbs.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Bootstrap Demo</title>
+
+	<!-- Bootstrap 5 -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<div class="container-fluid p-5 text-black text-center" style="background: linear-gradient(to right, #ffa726, #fb8c00, #f57c00)">
+		<h1>Halaman Web dengan Bootstrap</h1>
+		<p>Resize halaman responsive ini untuk melihat efeknya!</p>
+	</div>
+	<div class="container mt-5">
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>Kolom 1</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+					elit...</p>
+				<p>Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris...</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>Kolom 2</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+					elit...</p>
+				<p>Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris...</p>
+			</div>
+			<div class="col-sm-4">
+				<h3>Kolom 3</h3>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing
+					elit...</p>
+				<p>Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris...</p>
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>
---
+++ pbwci/app/Views/meeting.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-
+scale=1">
+	<title>Login</title>
+
+	<link rel="icon" href="<?= base_url('img/logo.png') ?>" type="image/png">
+
+	<!-- Bootstrap 5 -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+	<header class="navbar navbar-dark sticky-top bg-dark flex-md-
+nowrap p-0 shadow">
+		<div class="navbar-nav">
+			<div class="nav-item text-nowrap">
+				<a class="nav-link px-3" href="Signout">Sign out</a>
+			</div>
+		</div>
+	</header>
+	<div class="container-fluid">
+		Hello, <?= $username; ?>
+		<h1>Data Meeting</h1>
+	</div>
+</body>
+
+</html>
